675c94b39d2248f8db8c271ac9873495f98ca59f
dasFeuer <barunpanthisharma11@gmail.com>
2024-06-06 21:23:33 +0545

    added

---
+++ Main.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class Main{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter first integer: ");
+        int a = sc.nextInt();
+        System.out.print("Enter Second integer: ");
+        int b = sc.nextInt();
+        int sum = a + b;
+        System.out.print("The sum of given number is ");
+        System.out.println(sum);
+    }
+}
