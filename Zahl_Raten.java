fa9cb35e8d16781e5c545fca40ea531d3b5e3993
dasFeuer <barunpanthisharma11@gmail.com>
2024-06-25 08:13:07 +0545

    Zahl Raten GUI

--- Zahl_Raten.java
+++ Zahl_Raten.java
@@ -1,35 +1,76 @@
-import java.util.*;
+
+import java.awt.event.ActionEvent;
 import java.util.concurrent.ThreadLocalRandom;
+import javax.swing.*;
+// import java.awt.TextField;
 
 public class Zahl_Raten {
 
     static int myNumber = ThreadLocalRandom.current().nextInt(0, 100 + 1);
-    static  int tries = 0;
+    static int tries = 0;
+    static JLabel text = new JLabel("Gebe eine Zahl zwischen 0 und 100 ein!: ");
+    static JTextField textField = new JTextField();
+
     public static void guess(int number) {
-        if(number == myNumber) {
+        if (number == myNumber) {
             System.out.println("Richtig geraten!");
             System.out.println("Du hast " + tries + " Versuche gebraucht!");
-        } else {
-            System.out.println("Falsch geraten!");
+            text.setText("Richtig geraten mit " + tries + " Versuchen");
 
-            if(number < myNumber) {
-                System.out.println("Deine Zahl ist zu klein!\n");
+        } else {
+            tries++;
+            if (number < myNumber) {
+                text.setText("Falsch geraten! Deine Zahl ist zu klein!\n");
             } else {
-                System.out.println("Deine Zahl ist zu groß!\n");
+                text.setText("Falsch geraten! Deine Zahl ist zu groß!\n");
             }
-            nextRound();
+            textField.setText("");
+            //nextRound();
         }
     }
 
-    public static void nextRound() {
-        tries++;
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Bitte gebe eine Zahl ein: ");
-        int number = sc.nextInt();
-        guess(number);
+    // public static void nextRound() {
+    //     tries++;
+    //     Scanner sc = new Scanner(System.in);
+    //     System.out.println("Bitte gebe eine Zahl ein: ");
+    //     int number = sc.nextInt();
+    //     guess(number);
+    // }
+    public static void openUI() {
+        JFrame frame = new JFrame("Rate die Zahl!");
+        frame.setSize(400, 300);
+        frame.setLocation(100, 150);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        JFrame.setDefaultLookAndFeelDecorated(true);
+
+        text.setBounds(50, 50, 350, 30);
+
+        textField.setBounds(50, 100, 200, 30);
+
+        JButton button = new JButton("Raten!");
+        button.setBounds(50, 150, 200, 30);
+
+        button.addActionListener((ActionEvent e) -> {
+            try {
+                String textfromTextfield = textField.getText();
+                Integer number = Integer.valueOf(textfromTextfield);
+                guess(number);
+
+            } catch (NumberFormatException error) {
+                text.setText("Bitte gebe eine Zahl ein!");
+            }
+
+        });
+
+        frame.add(text);
+        frame.add(textField);
+        frame.add(button);
+        frame.setLayout(null);
+        frame.setVisible(true);
     }
 
     public static void main(String[] args) {
-        nextRound();
+        // nextRound();
+        openUI();
     }
 }
