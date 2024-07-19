a2688647c3fc200368db93c55c1ec512256503f1
dasFeuer <barunpanthisharma11@gmail.com>
2024-07-19 08:21:38 +0545

    Number add with array

---
+++ Arr.java
@@ -0,0 +1,31 @@
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Arr {
+    public static void addArr (int []input) {
+        int sum = 0;
+        for(int num: input) {
+            sum += num;
+        }
+        System.out.print("Sum of the " + Arrays.toString(input) + " are: " + sum);
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("How many numbers you want to print: ");
+        int size = sc.nextInt();
+        System.out.println("------------------------------------");
+        System.out.println("Please type 10 numbers to add: ");
+        int [] input = new int[size];
+
+        for (int i = 0; i < size; i++) {
+            input[i] = sc.nextInt();
+        }
+        addArr(input);
+        // int sum = 0;
+        // for(int num: input) {
+        //     sum += num;
+        // }
+        // System.out.println(sum);
+    }
+}
